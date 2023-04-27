64313d7daf691021fbc020ba4650a1559723ea2f
Michael Thrane <mwthrane@gmail.com>
2023-04-27 23:51:51 +0200

    r

--- excel.py
+++ excel.py
@@ -6,5 +6,6 @@ prod_list = inv_file["Sheet1"]
 prod_per_supplier = {}
 print(prod_list.max_row)
 
-for prod_row in range(2, prod_list.max_row +1)
-    supplier_name = prod_list.cell
+#for prod_row in range(2, prod_list.max_row +1)
+#    supplier_name = prod_list.cell
+
